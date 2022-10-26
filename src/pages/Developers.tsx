3394772882ef70c148ff806ccfd64217742fca87
SethRayMariano <112557701+SethRayMariano@users.noreply.github.com>
2022-10-26 19:18:17 +0800

    Update Developers.tsx

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -7,7 +7,15 @@ const Developers = () => {
         <div className='p-5 m-5 grid grid-cols-6 gap-2'>            
                 {/* Add your profile here. You can upload your image to the repo under "images"  directory
                     and link it here.                
-                */}
+                */
+                <div className="flex flex-wrap justify-center">
+                    <div className="w-64 sm:w-64 px-4">
+                        <img src="https://avatars.githubusercontent.com/u/112557701?v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                    </div>
+                    <h3 className='font-bold mr-1'>Seth Ray Mariano, </h3>
+                    <p className='font-thin italic'>Developer</p>
+                </div>      
+          }
                 {/* grid item */}
                 <div className="flex flex-wrap justify-center">
                     <div className="w-64 sm:w-64 px-4">
